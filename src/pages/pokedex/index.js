a8104ab31a9334ad3d70e184ad7d0b1fdcc6fc54
Lucas <l.duarte.mk@gmail.com>
2019-12-17 11:10:35 -0200

    initial commit and config

---
+++ src/pages/pokedex/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Pokedex = () => <h1>pokedex</h1>;
+
+export default Pokedex;
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import List from '../pages/list';
+import Pokedex from '../pages/pokedex';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/list" component={List} />
+      <Route path="/pokedex" component={Pokedex} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
