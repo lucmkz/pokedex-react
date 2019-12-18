f40ba07e684f75f9a919e5824908bc8a7c6b2c80
Lucas <l.duarte.mk@gmail.com>
2019-12-18 20:05:34 -0200

    routes render by /

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
-import List from '../pages/list';
+// import List from '../pages/list';
 import Pokedex from '../pages/pokedex';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/list" component={List} />
-      <Route path="/pokedex" component={Pokedex} />
+      {/* <Route path="/list" component={List} /> */}
+      <Route path="/" component={Pokedex} />
     </Switch>
   </BrowserRouter>
 );
