f8fc02f20d87791123b1a6bc90a4ce593fc711eb
Lucas <l.duarte.mk@gmail.com>
2019-12-19 10:56:09 -0200

    Updates

---
+++ precache-manifest.d9dfd11d5da0f480fa78f0fd491bb367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001361148589b491a1a305943ccc1c29",
+    "url": "/pokedex-react/index.html"
+  },
+  {
+    "revision": "2bc43851549229191451",
+    "url": "/pokedex-react/static/js/2.2b8132a5.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/pokedex-react/static/js/2.2b8132a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c64f56a77d4a798fb0b",
+    "url": "/pokedex-react/static/js/main.b703aaac.chunk.js"
+  },
+  {
+    "revision": "eeee0ee2709e6d93daa1",
+    "url": "/pokedex-react/static/js/runtime-main.859d6295.js"
+  },
+  {
+    "revision": "6058de967fa87c0ef32dbaa253eb2274",
+    "url": "/pokedex-react/static/media/pokeball.6058de96.svg"
+  },
+  {
+    "revision": "dda38142c32c66ec0801a94de574edb8",
+    "url": "/pokedex-react/static/media/pokeball_icon.dda38142.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d72c1d6d99fcb6cfc1f26b15db2bb9fc.js"
+  "/pokedex-react/precache-manifest.d9dfd11d5da0f480fa78f0fd491bb367.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
