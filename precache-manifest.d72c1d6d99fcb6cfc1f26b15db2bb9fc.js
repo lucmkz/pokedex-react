8047a02879777bd3f759438a291f1220f3e8ef14
Lucas <l.duarte.mk@gmail.com>
2019-12-19 10:43:56 -0200

    Updates

---
+++ precache-manifest.d72c1d6d99fcb6cfc1f26b15db2bb9fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e463d3cbaad82c9ae7d6a322dea8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0ff09f20ac87fba689c",
+    "url": "/static/js/2.a8b13ea3.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/static/js/2.a8b13ea3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c64f56a77d4a798fb0b",
+    "url": "/static/js/main.b703aaac.chunk.js"
+  },
+  {
+    "revision": "f5860e3ac1214ca9d6a5",
+    "url": "/static/js/runtime-main.05cd4dee.js"
+  },
+  {
+    "revision": "6058de967fa87c0ef32dbaa253eb2274",
+    "url": "/static/media/pokeball.6058de96.svg"
+  },
+  {
+    "revision": "dda38142c32c66ec0801a94de574edb8",
+    "url": "/static/media/pokeball_icon.dda38142.svg"
+  }
+]);
