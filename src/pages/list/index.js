1205114da721f9d5fb17248a5da5140e513e8aca
Lucas <l.duarte.mk@gmail.com>
2019-12-20 17:54:11 -0200

    poke on list and on state

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -6,20 +6,29 @@ const List = () => {
   const [repository, setRepository] = useState([]);
 
   // setRepository(async () => {});
-
-  const req = async () => {
-    const res = await api.get('1');
-
-    setRepository({
-      repository: res,
-    });
-
-    console.log('response', res);
+  const arr = [];
+  const req = async i => {
+    const res = await api.get(i.toString());
+    arr.push(res);
+    // console.log('response', res);
   };
 
-  useEffect(() => req(), []);
+  console.log(arr);
 
-  const a = 0;
+  useEffect(() => {
+    for (let i = 1; i < 802; i++) {
+      req(i);
+    }
+  }, []);
+
+  useEffect(() => {
+    setRepository({
+      repository: arr,
+    });
+    console.log('repositoryr', repository);
+  }, []);
+
+  console.log(repository);
 
   return (
     <Card>
